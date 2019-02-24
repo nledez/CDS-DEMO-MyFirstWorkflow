35858d929f3793989dcc5ce495962b76891e3cab
Nicolas Ledez <github.public@ledez.net>
2019-02-25 00:37:43 +0100

    Add deps to tests

--- make_test.sh
+++ make_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 go get github.com/gorilla/mux
 go get github.com/justinas/alice
+go get github.com/stretchr/testify/assert
 
 go test -cover -coverprofile=coverage.out -v 2>&1 | tee test.log
 go tool cover -html=coverage.out -o coverage.html
